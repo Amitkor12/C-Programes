4fc55510ec97a4bdc8cf2657dbc5e5c8b884d934
Amit <amitcor12@gmail.com>
2023-01-06 20:44:10 +0530

    callback calculation

--- Callbach_func.c
+++ Callbach_func.c
@@ -1,25 +1,50 @@
+// #include <stdio.h>
+
+
+// void calback_fn(void)
+// {
+//     printf("A callback Function\n");
+// }
+
+// void funtion_test(void (*op)(void))
+// {
+//     for(int i = 0;i<3;i++)
+//     {
+//         if(i == 1)
+//         {
+//             (*op) ();
+//         }
+//         printf("i = %d\n",i);
+//     }
+// }
+
+// void main()
+// {
+//     void (*op_ptr)(void) = &calback_fn;
+//     funtion_test(op_ptr);
+// }
+
+
+
+
+/***********************
+ *  CallBack to calculate
+*****************/
+
 #include <stdio.h>
+#include <math.h>
 
-
-void calback_fn(void)
+float square_of_num(int x)
 {
-    printf("A callback Function\n");
+    return x*x;
 }
 
-void funtion_test(void (*op)(void))
+float calculate(int a,float (*op) (int))
 {
-    for(int i = 0;i<3;i++)
-    {
-        if(i == 1)
-        {
-            (*op) ();
-        }
-        printf("i = %d\n",i);
-    }
+    (* op)(a);
 }
 
 void main()
 {
-    void (*op_ptr)(void) = &calback_fn;
-    funtion_test(op_ptr);
+    printf("%f\n",calculate(4,square_of_num));
 }
